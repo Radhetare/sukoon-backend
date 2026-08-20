d1df68b21bd59fd4a7a3ab8f18e5dcbacf6dfd5c
Radhe Tare <khushtare61205@gmail.com>
2026-08-20 21:18:47 +0530

    Change GROQ_MODEL

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ import os
 class Settings(BaseSettings):
     APP_NAME: str = "Sukoon"
     GROQ_API_KEY: str
-    GROQ_MODEL: str = "llama-3.1-8b-instant"
+    GROQ_MODEL: str = "openai/gpt-oss-20b"
     MONGODB_URL: str
 
     model_config = SettingsConfigDict(env_file=".env")
